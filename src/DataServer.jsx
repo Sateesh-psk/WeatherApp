11c8102b6aae3d1c413def7c36c3080d4432caaa
Sateesh-psk <123976252+Sateesh-psk@users.noreply.github.com>
2023-05-16 16:49:02 +0530

    Update DataServer.jsx

--- src/DataServer.jsx
+++ src/DataServer.jsx
@@ -4,7 +4,7 @@ import Weather from "./Weather";
 
 function Data(props){
     const [weather,setWeather]=useState();
-    let status=200
+    let status=""
     const key="http://api.weatherapi.com/v1/forecast.json?key=1ffb767f8d7140cb877102609232004&q="+props.city+"&days=1&aqi=yes&alerts=no";
     useEffect(()=>{
         axios.get(key)
